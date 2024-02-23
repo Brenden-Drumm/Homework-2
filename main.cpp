b920cabbd2e58162d9a88c9a21b734f2b71ef258
Brenden-Drumm <158791171+Brenden-Drumm@users.noreply.github.com>
2024-02-23 16:58:54 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,82 @@
+/*************************************************************************
+** Author : Brenden Drumm
+** Program : hw2, q2
+** Date Created : February 23, 2024
+** Date Last Modified : February 23, 2024
+** Usage : No command line arguments
+**
+** Problem:
+Accept the following information from the user (keyboard):
+- Hw1, hw2 and hw3 (out of 100)
+- Midterm (out of 100)
+- Final exam (out of 100)
+Calculate the total grade out of 100 based on the following grading scale:
+Hws --> 30% (10% each)
+Midterm --> 30%
+Final Exam --> 40%
+*************************************************************************/
+#include <iostream>
+using namespace std;
+
+struct Check{
+    int CheckNum;
+    string CheckMemo;
+    int CheckAmount;
+};
+
+class Checkbook{
+public:
+    Checkbook():balance(0), numOfChecks(0){}
+    Checkbook(int initBal):balance(initBal), numOfChecks(0){}
+
+    void deposit(float depositAmount);
+    void displayChecks();
+    bool writeCheck(float amount);
+private:
+    Check checkArray[5];
+    float balance;
+    float lastDeposit;
+    int numOfChecks;
+    int checkLimit;
+};
+
+void Checkbook::deposit(float depositAmount){
+    balance += depositAmount;
+}
+
+void Checkbook::displayChecks(){
+    for(int i = 0; i < numOfChecks; i++){
+        cout<<"Check "<<i<<" Memo "<<checkArray[i].CheckMemo<<endl;
+        cout<<"Check "<<i<<" Amount "<<checkArray[i].CheckAmount<<endl;
+    }
+
+}
+
+bool Checkbook::writeCheck(float amount){
+
+    if(amount<balance){
+        checkArray[numOfChecks].CheckAmount = amount;
+        cout<<"Check memo?"<<endl;
+        cin>>checkArray[numOfChecks].CheckMemo;
+        balance-=amount;
+        cout<<"New Balance: "<<balance<<endl;
+        numOfChecks++;
+    }
+    else{
+        cout<<"exceeding balance"<<endl;
+        return false;
+    }
+}
+
+
+int main()
+{
+    cout << "Hello world!" << endl;
+
+    Checkbook cheque(500);
+    cheque.writeCheck(200);
+    cheque.writeCheck(100);
+    cheque.displayChecks();
+
+    return 0;
+}
